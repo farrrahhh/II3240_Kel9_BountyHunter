0d279ce02c862abf11dcb199643feee7095fccbf
IrfanMusthofa <87060699+IrfanMusthofa@users.noreply.github.com>
2025-05-26 11:14:13 +0700

    update eslint ignore

--- pwa-user/next.config.ts
+++ pwa-user/next.config.ts
@@ -3,15 +3,15 @@ const withPWA = require('next-pwa')({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
-  eslint: {
-    ignoreDuringBuilds: true,
-  }
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   // config lainnya kalau ada
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = withPWA(nextConfig);
